b71ec03d9884ec1e8828b177f4dbdd7cef521307
Korey Corkscrew <77598913+korey-corkscrew@users.noreply.github.com>
2022-09-19 12:06:25 -0500

    finished

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,12 +1,14 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const Lock = await ethers.getContractFactory("PuzzleWalletHack");
-  const lock = await Lock.deploy({gasLimit: 5000000, value: ethers.utils.parseEther("0.001")});
+  const Lock = await ethers.getContractFactory("DoubleEntryPointHack");
+  const lock = await Lock.deploy({gasLimit: 5000000});
 
   await lock.deployed();
 
   console.log("Lock with 1 ETH deployed to:", lock.address);
+
+  // await lock.exec({gasLimit: 1000000});
 }
 
 // We recommend this pattern to be able to use async/await everywhere
